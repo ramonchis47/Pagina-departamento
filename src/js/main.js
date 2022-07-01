66a77fbc595e977aa8996e43a85666987f845a20
Erick <erickramon47@live.com.mx>
2022-06-30 20:55:24 -0500

    Se hace responsivo hasta cierto punto

---
+++ src/js/main.js
@@ -0,0 +1,19 @@
+// Para mostrar el primer dropwmenu
+const menuMas = document.querySelector("#bmas")
+const menu_A_revelar = document.querySelector(".mas .menu-mas")
+
+menuMas.addEventListener("click", (e) => {
+    e.preventDefault()
+
+    menu_A_revelar.classList.toggle("show")
+})
+
+// Para mostrar el segundo dropwmenu
+const menuMas2 = document.querySelector("#bmas2")
+const menu_A_revelar2 = document.querySelector(".mas2 .menu-mas2")
+
+menuMas2.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    menu_A_revelar2.classList.toggle("show")
+})
